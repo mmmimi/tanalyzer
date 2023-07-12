4908b631f51544e60c8424328111b5bb39a95702
jorge-martinez-gil <34342266+jorge-martinez-gil@users.noreply.github.com>
2023-07-12 15:27:35 +0200

    New changes

--- app.py
+++ app.py
@@ -3,45 +3,48 @@ import geopandas as gpd
 import pandas as pd
 import streamlit as st
 
-# Set up the Streamlit app
-st.title("Smart CommUnity - TA Analyzer")
-lat = st.number_input("Enter the latitude of the area:", value=48.34)
-lon = st.number_input("Enter the longitude of the area:", value=14.49)
-zoom = st.slider("Zoom level:", min_value=1, max_value=18, value=10)
+def main():
+    # Set up the Streamlit app
+    st.title("Smart CommUnity - TA Analyzer")
+    lat = st.number_input("Enter the latitude of the area:", value=48.34)
+    lon = st.number_input("Enter the longitude of the area:", value=14.49)
+    zoom = st.slider("Zoom level:", min_value=1, max_value=18, value=10)
 
-# Allow the user to select the data to display
-show_buildings = st.checkbox("Show Buildings")
-show_amenities = st.checkbox("Show Amenities")
+    # Allow the user to select the data to display
+    show_buildings = st.checkbox("Show Buildings")
+    show_amenities = st.checkbox("Show Amenities")
 
-# Retrieve the graph from OpenStreetMap
-G = ox.graph_from_point((lat, lon), network_type='all', dist=1000)
+    # Retrieve the graph from OpenStreetMap
+    G = ox.graph_from_point((lat, lon), network_type='all', dist=1000)
 
-# Get a list of the features (buildings and/or amenities)
-features = []
+    # Get a list of the features (buildings and/or amenities)
+    features = []
 
-if show_buildings:
-    buildings = ox.geometries_from_point((lat, lon), tags={'building': True}, dist=1000)
-    features.append(buildings)
+    if show_buildings:
+        buildings = ox.features_from_point((lat, lon), tags={'building': True}, dist=1000)
+        features.append(buildings)
 
+    if show_amenities:
+        try:
+            amenities = ox.features_from_point((lat, lon), tags={'amenity': True}, dist=1000)
+            features.append(amenities)
+        except:
+            st.warning("No amenities found within the specified distance.")
 
-if show_amenities:
-    try:
-        amenities = ox.geometries_from_point((lat, lon), tags={'amenity': True}, dist=1000)
-        features.append(amenities)
-    except:
-        st.warning("No amenities found within the specified distance.")
+    # Concatenate the features GeoDataFrames
+    if len(features) > 0:
+        features = gpd.GeoDataFrame(pd.concat(features, ignore_index=True), crs="EPSG:4326")
 
-# Concatenate the features GeoDataFrames
-if len(features) > 0:
-    features = gpd.GeoDataFrame(pd.concat(features, ignore_index=True), crs="EPSG:4326")
+    # Plot the graph using OSMnx
+    fig, ax = ox.plot_graph(G, show=False, close=False, edge_color='gray', edge_linewidth=1, edge_alpha=0.5, figsize=(10, 10))
+    ax.set_title('Visualization')
 
-# Plot the graph using OSMnx
-fig, ax = ox.plot_graph(G, show=False, close=False, edge_color='gray', edge_linewidth=1, edge_alpha=0.5, figsize=(10, 10))
-ax.set_title('Visualization')
+    # Plot the features (buildings and/or amenities)
+    if len(features) > 0:
+        features.plot(ax=ax, color='red', alpha=0.7)
 
-# Plot the features (buildings and/or amenities)
-if len(features) > 0:
-    features.plot(ax=ax, color='red', alpha=0.7)
+    # Display the plot in the Streamlit app
+    st.pyplot(fig)
 
-# Display the plot in the Streamlit app
-st.pyplot(fig)
+if __name__ == "__main__":
+    main()

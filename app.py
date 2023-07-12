918e5bd1a345e402bde6f4a44ebc420b4ef29a8d
jorge-martinez-gil <34342266+jorge-martinez-gil@users.noreply.github.com>
2023-07-12 13:17:16 +0200

    New changes

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 # Set up the Streamlit app
-st.title("SmartCommUnity - TA Analyzer")
-lat = st.number_input("Enter the latitude of the area:", value=48.36)
-lon = st.number_input("Enter the longitude of the area:", value=14.51)
+st.title("OpenStreetMap Data Visualization")
+lat = st.number_input("Enter the latitude of the area:", value=48.34)
+lon = st.number_input("Enter the longitude of the area:", value=14.49)
 zoom = st.slider("Zoom level:", min_value=1, max_value=18, value=10)
 
 # Allow the user to select the data to display
@@ -18,11 +18,11 @@ show_amenities = st.checkbox("Show Amenities")
 G = ox.graph_from_point((lat, lon), network_type='all', dist=1000)
 
 # Get a list of the features (buildings and/or amenities)
-features = gpd.GeoDataFrame()
+features = []
 
 if show_buildings:
     buildings = ox.geometries_from_point((lat, lon), tags={'building': True}, dist=1000)
-    features = features.append(buildings)
+    features.append(buildings)
 
 if show_amenities:
     amenities = ox.geometries_from_point((lat, lon), tags={'amenity': True}, dist=1000)

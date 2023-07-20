9502205db16244b2ada5f9cb859852027e338876
jorge-martinez-gil <34342266+jorge-martinez-gil@users.noreply.github.com>
2023-07-20 08:20:26 +0200

    New functionality added

--- app.py
+++ app.py
@@ -29,29 +29,71 @@ def main():
     selected_coordinate = example_coordinates[example_choice]
     lat = st.number_input("Enter the latitude of the area:", value=selected_coordinate[0])
     lon = st.number_input("Enter the longitude of the area:", value=selected_coordinate[1])
-    zoom = st.slider("Zoom level:", min_value=1, max_value=18, value=10)
-
+    zoom = st.slider("Zoom level:", min_value=1, max_value=10, value=5)
+    
+    dista = 200 * zoom
 
     # Allow the user to select the data to display
-    show_buildings = st.checkbox("Show Buildings")
-    show_amenities = st.checkbox("Show Amenities")
+    st.write("Select data to display:")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        show_buildings = st.checkbox("Show Buildings")
+
+        show_amenities = st.checkbox("Show Amenities")
+
+    with col2:
+        show_emergencies = st.checkbox("Show Emergencies")
+
+        show_commercial_land = st.checkbox("Show Commercial Land")
+        
+    # Define colors for each feature type
+    feature_colors = {
+        "building": "red",
+        "amenity": "green",
+        "emergency": "blue",
+        "commercial_land": "yellow"
+    }
 
     # Retrieve the graph from OpenStreetMap
-    G = ox.graph_from_point((lat, lon), network_type='all', dist=1000)
+    G = ox.graph_from_point((lat, lon), network_type='all', dist=dista)
 
     # Get a list of the features (buildings and/or amenities)
     features = []
 
     if show_buildings:
-        buildings = ox.geometries_from_point((lat, lon), tags={'building': True}, dist=1000)
-        features.append(buildings)
+        try:
+            buildings = ox.geometries_from_point((lat, lon), tags={'building': True}, dist=1000)
+            buildings["feature_type"] = "building"  # Add a new column to specify the feature type
+            features.append(buildings)
+        except:
+            st.warning("No buildings found within the specified distance.")
 
     if show_amenities:
         try:
             amenities = ox.geometries_from_point((lat, lon), tags={'amenity': True}, dist=1000)
+            amenities["feature_type"] = "amenity"  # Add a new column to specify the feature type
             features.append(amenities)
         except:
             st.warning("No amenities found within the specified distance.")
+            
+    if show_emergencies:
+        try:
+            emergencies = ox.geometries_from_point((lat, lon), tags={'emergency': True}, dist=1000)
+            emergencies["feature_type"] = "emergency"  # Add a new column to specify the feature type
+            features.append(emergencies)
+        except:
+            st.warning("No emergencies found within the specified distance.")
+    
+    if show_commercial_land:
+        try:
+            commercial_land = ox.geometries_from_point((lat, lon), tags={'landuse': 'commercial'}, dist=1000)
+            commercial_land["feature_type"] = "commercial_land"  # Add a new column to specify the feature type
+            features.append(commercial_land)
+        except:
+            st.warning("No commercial land found within the specified distance.")
+
+
 
     # Concatenate the features GeoDataFrames
     if len(features) > 0:
@@ -61,9 +103,12 @@ def main():
     fig, ax = ox.plot_graph(G, show=False, close=False, edge_color='gray', edge_linewidth=1, edge_alpha=0.5, figsize=(10, 10))
     ax.set_title('Visualization')
 
-    # Plot the features (buildings and/or amenities)
-    if len(features) > 0:
-        features.plot(ax=ax, color='red', alpha=0.7)
+    # Plot the features (buildings and/or amenities) with different colors
+    for feature_type, color in feature_colors.items():
+        if len(features) > 0:
+            filtered_features = features[features["feature_type"] == feature_type]
+            if not filtered_features.empty:
+                filtered_features.plot(ax=ax, color=color, alpha=0.7)
 
     # Display the plot in the Streamlit app
     st.pyplot(fig)
